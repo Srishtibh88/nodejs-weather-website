ee3273bffade06ea59d7c6b79e3e6b32fbcdfb0d
Srishti Bhatia <srishtibh88@gmail.com>
2020-10-09 21:13:42 +0530

    About page and weather data changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,8 @@ const forecast = (lat, long, callback) => {
         }else if(response.body.error){
             callback('Unable to find location', undefined)
         }else {
-            callback(undefined, response.body.current.weather_descriptions[0] + '. Its ' + response.body.current.temperature + ' degrees out, feels like ' 
-                                +  response.body.current.feelslike + ' degrees' + '. Chances of rain ' + response.body.current.precip + '%.')
+            callback(undefined, response.body.current.weather_descriptions[0] + '. Its ' + response.body.current.temperature + ' degrees out. It feels like ' 
+                                +  response.body.current.feelslike + ' degrees out. There are ' + response.body.current.precip + '% chances of rain. The humidity is ' + response.body.current.humidity + '%.')
         }
     })
 
